c962c2a1c0fafa593f89e07edf591ce87fb6fcdb
jifsvbi <joaopedro222931@gmail.com>
2025-05-27 11:13:08 -0300

    comitado

---
+++ pages/areas/script.js
@@ -0,0 +1,14 @@
+        document.addEventListener('DOMContentLoaded', async function() {
+          const area = "mistieri";
+          const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:3000/brinquedos/area${area}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "authorization": "bearer " + token
+                },
+            });
+            
+            const data = await response.json();
+            console.log(data);
+        });
