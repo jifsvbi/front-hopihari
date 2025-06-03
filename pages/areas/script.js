3a3642917a40b132e85b058adb3fe39bb4dd1072
jifsvbi <joaopedro222931@gmail.com>
2025-06-03 17:01:32 -0300

    fazendo a rota area

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -1,7 +1,8 @@
         document.addEventListener('DOMContentLoaded', async function() {
-          const area = "mistieri";
-          const token = localStorage.getItem('token');
-            const response = await fetch(`http://localhost:3000/brinquedos/area${area}`, {
+          const urlParams = new URLSearchParams(window.location.search);
+          const area = urlParams.get('area');
+          const token = String(localStorage.getItem('token').replaceAll('"', ''));
+          const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -11,4 +12,20 @@
             
             const data = await response.json();
             console.log(data);
+
+            for (var brinquedo of data.resultados) {
+                console.log(brinquedo);
+
+                const grid = document.querySelector(".rides-grid");
+                grid.innerHTML = grid.innerHTML + `
+                <div class="ride-card">
+                <div class="ride-image" style="background-image: url('imagem/Montezum.jpg')"></div>
+                <div class="ride-info">
+                    <h3 class="ride-name">${brinquedo.name}</h3>
+                    <div class="ride-time">${brinquedo.waiting_time}</div>
+                    <span class="ride-status status-busy">${brinquedo.status}</span>
+                </div>
+            </div>
+            `
+            }
         });
